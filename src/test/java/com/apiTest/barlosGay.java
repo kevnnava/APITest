9bb8d3dfb60c6b043d34ec5fcc1e657c870a8ec0
kevinnava <kevin.nava00@gmail.com>
2022-09-12 15:23:05 -0500

    commit

--- src/test/java/com/apiTest/barlosGay.java
+++ src/test/java/com/apiTest/barlosGay.java
@@ -1,4 +1,18 @@
 package com.apiTest;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class barlosGay {
+
+    String url = "http://3.208.29.145:8000";
+
+    @DisplayName("GET all Spartans")
+    @Test
+    public void test1(){
+
+    }
 }
